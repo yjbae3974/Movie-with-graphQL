899a671b590db741aba95cb5b2a7cbdbf729bb99
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-11-09 23:16:56 +0900

    made apollo movie baseset

---
+++ movie/src/components/App.js
@@ -0,0 +1,13 @@
+import {HashRouter as Router, Route} from 'react-router-dom';
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
+function App() {
+  return (
+    <Router>
+      <Route exact path ="/" component={Home}></Route>
+      <Route path ="/:id" component={Detail}></Route>
+    </Router>
+  );
+}
+
+export default App;
